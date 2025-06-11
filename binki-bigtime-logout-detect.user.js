322c4a67b4e0073987a78d4e7244ba709d859c20
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2025-06-11 09:33:31 -0400

    Fix @match to support domains other than intuit.bigtime.net.
    Fixes #2.

--- binki-bigtime-logout-detect.user.js
+++ binki-bigtime-logout-detect.user.js
@@ -2,13 +2,17 @@
 // @name binki-bigtime-logout-detect
 // @version  1.0.0
 // @homepageURL https://github.com/binki/binki-bigtime-logout-detect
-// @match https://intuit.bigtime.net/bigtime
-// @match https://intuit.bigtime.net/bigtime/*
-// @match https://intuit.bigtime.net/Bigtime
-// @match https://intuit.bigtime.net/Bigtime/*
+// @match https://*.bigtime.net/bigtime
+// @match https://*.bigtime.net/bigtime/*
+// @match https://*.bigtime.net/Bigtime
+// @match https://*.bigtime.net/Bigtime/*
 // ==/UserScript==
 
 (async () => {
+  // We are forced to use wildcard @match above but we don’t want to match irrelevant subdomains.
+  // The only subdomain which has a fixed alternative purpose that we know about so far is “www.bigtime.net”,
+  // so test for that. See #2.
+  if (/^[^:]+:\/\/www\./.test(document.URL)) return;
   // I tried using jQuery via unsafeWindow, but that had errors. For
   // this, we need to hook into the jQuery that the site itself is
   // using since that lets us intercept AJAX requests/responses. So
